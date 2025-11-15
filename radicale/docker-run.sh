eff5b47fca2b080a94297d174e4dd50399be7d49
Exocen <Exocen@users.noreply.github.com>
2025-11-14 22:55:25 -0700

    640K ought to be enough for anybody

--- radicale/docker-run.sh
+++ radicale/docker-run.sh
@@ -3,17 +3,21 @@
 if [ "$(id -u)" -ne 0 ]; then
     echo "Must be run as root"
     exit 1
-else
-    if [ -z ${RADICALE_PATH+x} ] ; then
-        if [ -z "$1" ]; then
-            RADICALE_PATH="/docker-data/radicale/"
-        fi
-    fi
+fi
+
+cd "$(dirname "$0")"
+
+mkdir -p /docker-data/radicale/config
+mkdir -p /docker-data/radicale/data
+
+if [ ! -e "/docker-data/radicale/config/config" ]; then
+    cp config /docker-data/radicale/config/
 fi
 
 docker run -d --rm --log-driver=journald --log-opt tag="{{.Name}}" \
     --name=radicale \
     -v /etc/timezone:/etc/timezone:ro -v /etc/localtime:/etc/localtime:ro \
     --net user_network --ip 10.0.0.83 \
-    -v "$RADICALE_PATH":/data \
+    -v /docker-data/radicale/data:/data \
+    -v /docker-data/radicale/config:/config:ro \
     tomsquest/docker-radicale && echo "radicale started."
