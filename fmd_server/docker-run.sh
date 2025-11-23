7161b52652e4c38d7707aaf7642e6d2133afb524
Exocen <Exocen@users.noreply.github.com>
2025-11-22 20:07:40 -0700

    s/    /  /g

--- fmd_server/docker-run.sh
+++ fmd_server/docker-run.sh
@@ -5,9 +5,14 @@ if [ "$(id -u)" -ne 0 ]; then
     exit 1
 fi
 
+DOCKER_PATH="/docker-data/fmt-server/db"
+
+cd "$(dirname "$(readlink -f "$0")")" || exit 1
+mkdir -p $DOCKER_PATH
+
 docker run -d --rm --log-driver=journald --log-opt tag="{{.Name}}" \
     -v /etc/timezone:/etc/timezone:ro -v /etc/localtime:/etc/localtime:ro \
-    -v /docker-data/fmd-server/:/var/lib/fmd-server/db/ \
+    -v "$DOCKER_PATH":/var/lib/fmd-server/db/ \
     --net user_network --ip 10.0.0.83 \
     --name=fmd_server \
     registry.gitlab.com/fmd-foss/fmd-server && echo "fmd_server started."
