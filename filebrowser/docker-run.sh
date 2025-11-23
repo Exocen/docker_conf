e11f26174ef04908d84863b9be3bae1e15b2c3fd
Exocen <Exocen@users.noreply.github.com>
2025-11-22 17:53:08 -0700

    this is how we generate our shit.

--- filebrowser/docker-run.sh
+++ filebrowser/docker-run.sh
@@ -23,14 +23,11 @@ mkdir -p $DOCKER_PATH
 
 if [ ! -f "$FILEBROWSER_SETTINGS_PATH" ] ; then
     mkdir $FILEBROWSER_SETTINGS_PATH
-    chown 1000:1000 $FILEBROWSER_SETTINGS_PATH
     echo -en '{\n    "port": 80,\n    "baseURL": "",\n    "address": "",\n    "log": "stdout",\n    "database": "/database.db",\n    "root": "/srv"\n}' > $FILEBROWSER_SETTINGS_PATH/settings.json
-    chown 1000:1000 $FILEBROWSER_SETTINGS_PATH/settings.json
 fi
 
 if [ ! -f "$FILEBROWSER_DB_PATH" ] ; then
     mkdir $FILEBROWSER_DB_PATH
-    chown 1000:1000 $FILEBROWSER_DB_PATH
 fi
 
 #UserId:GroudId -> 1000:1000 must have folder permission
