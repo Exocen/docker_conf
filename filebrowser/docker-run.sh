75c699abaf67e2402e24e12c14ef94fd5b83c889
Exocen <Exocen@users.noreply.github.com>
2025-11-22 18:54:57 -0700

    8==========D

--- filebrowser/docker-run.sh
+++ filebrowser/docker-run.sh
@@ -14,28 +14,14 @@ else
     fi
 fi
 
-DOCKER_PATH="/docker-data/filebrowser/"
-FILEBROWSER_DB_PATH="$DOCKER_PATH/filebrowser.db"
-FILEBROWSER_SETTINGS_PATH="$DOCKER_PATH/config"
+DOCKER_PATH="/docker-data/filebrowser"
 
 cd "$(dirname "$(readlink -f "$0")")" || exit 1
-mkdir -p $DOCKER_PATH
-
-if [ ! -f "$FILEBROWSER_SETTINGS_PATH" ] ; then
-    mkdir $FILEBROWSER_SETTINGS_PATH
-    echo -en '{\n    "port": 80,\n    "baseURL": "",\n    "address": "",\n    "log": "stdout",\n    "root": "/srv"\n}' > $FILEBROWSER_SETTINGS_PATH/settings.json
-fi
-
-if [ ! -f "$FILEBROWSER_DB_PATH" ] ; then
-    touch $FILEBROWSER_DB_PATH
-    chown 1000:1000 $FILEBROWSER_DB_PATH
-fi
 
 #UserId:GroudId -> 1000:1000 must have folder permission
 docker run \
     --name filebrowser --log-driver=journald --log-opt tag="{{.Name}}" --rm -d \
-    -v $FILEBROWSER_DB_PATH:/filebrowser.db \
-    -v $FILEBROWSER_SETTINGS_PATH:/config \
+    -e FB_NOAUTH=true \
     -v "$FILEBROWSER_PATH":/srv \
     -u 1000:1000 \
     -p 80:80 \
