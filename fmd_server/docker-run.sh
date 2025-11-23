202f9a7d09c2711c76ff4174c6dcdcb32f7bbe87
Exocen <Exocen@users.noreply.github.com>
2025-11-22 20:01:42 -0700

    Oh no

--- fmd_server/docker-run.sh
+++ fmd_server/docker-run.sh
@@ -7,6 +7,7 @@ fi
 
 docker run -d --rm --log-driver=journald --log-opt tag="{{.Name}}" \
     -v /etc/timezone:/etc/timezone:ro -v /etc/localtime:/etc/localtime:ro \
+    -v /docker-data/fmd-server/:/var/lib/fmd-server/db/ \
     --net user_network --ip 10.0.0.83 \
     --name=fmd_server \
     registry.gitlab.com/fmd-foss/fmd-server && echo "fmd_server started."
