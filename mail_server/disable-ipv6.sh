59c996d6b19e3a4c74433121468529c85988e213
Exocen <Exocen@users.noreply.github.com>
2025-12-02 23:12:48 -0700

    rats

--- mail_server/disable-ipv6.sh
+++ mail_server/disable-ipv6.sh
@@ -6,6 +6,7 @@ if ip a | grep inet6; then
 
     echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
     echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
+    echo "net.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.conf
 
     sysctl -p
 
