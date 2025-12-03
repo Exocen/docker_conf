6da44234243bc1828444cc294e42851b68df21a7
Exocen <Exocen@users.noreply.github.com>
2025-12-02 23:23:02 -0700

    This solves it.

--- mail_server/disable-ipv6.sh
+++ mail_server/disable-ipv6.sh
@@ -4,9 +4,9 @@ if ip a | grep inet6; then
 
     echo "Ipv6 detected, disabling"
 
-    echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
-    echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
-    echo "net.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.conf
+    echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.d/local.conf
+    echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.d/local.conf
+    echo "net.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.d/local.conf
 
     sysctl -p
 
