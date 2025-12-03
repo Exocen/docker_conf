ce991f989bdf2ee6fb857a5c8552908ba9a75edc
Exocen <Exocen@users.noreply.github.com>
2025-12-03 15:42:55 -0700

    Arrrrgggg

--- mail_server/disable-ipv6.sh
+++ mail_server/disable-ipv6.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-if ip a | grep inet6; then
-
+#if ip a | grep inet6; then
+if [ `cat /proc/sys/net/ipv6/conf/all/disable_ipv6` -ne 1 ]; then
     echo "Ipv6 detected, disabling"
-
-    echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.d/local.conf
-    echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.d/local.conf
-    echo "net.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.d/local.conf
-
+    file_path="/etc/sysctl.d/ipv6-disabled"
+    rm -f $file_path
+    echo "net.ipv6.conf.all.disable_ipv6 = 1" >> $file_path
+    echo "net.ipv6.conf.default.disable_ipv6 = 1" >> $file_path
+    echo "net.ipv6.conf.lo.disable_ipv6 = 1" >> $file_path
     sysctl -p
-
 fi
 
-ip a | grep inet6 && echo "ipv6 still active !" || echo "ipv6 disabled"
+if [ `cat /proc/sys/net/ipv6/conf/all/disable_ipv6` -eq 1 ]; then
+    echo "ipv6 disabled"
+else
+    echo "ipv6 still active !"
+    exit 1
+fi
