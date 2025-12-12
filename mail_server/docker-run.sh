4bc935e7459e1ccd77c10eb3ef9a2de3a0d6f6ff
Exocen <Exocen@users.noreply.github.com>
2025-12-12 10:50:46 -0700

    It's secret!

--- mail_server/docker-run.sh
+++ mail_server/docker-run.sh
@@ -25,7 +25,7 @@ docker run -d --rm --log-driver=journald --log-opt tag="{{.Name}}" \
     -v /docker-data/dms/mail-data:/var/mail -v /docker-data/dms/mail-state:/var/mail-state \
     -v /docker-data/dms/mail-logs:/var/log/mail -v /docker-data/dms/config:/tmp/docker-mailserver \
     -v /docker-data/letsencrypt:/etc/letsencrypt \
-    -p 25:25 -p 464:465 -p 992:993 -p 4190:4190\
+    -p 25:25 -p 465:465 -p 993:993 -p 4190:4190\
     -e ENABLE_FAIL2BAN=1 -e SSL_TYPE=letsencrypt -e PERMIT_DOCKER=network \
     -e ONE_DIR=1 -e ENABLE_CLAMAV=0 -e ENABLE_SPAMASSASSIN=1 -e SPOOF_PROTECTION=1 -e ENABLE_MANAGESIEVE=1 \
     -e POSTMASTER_ADDRESS="$POSTMASTER_ADDRESS" -e ENABLE_UPDATE_CHECK=1 -e PFLOGSUMM_TRIGGER="logrotate" -e LOGWATCH_INTERVAL="daily" \
